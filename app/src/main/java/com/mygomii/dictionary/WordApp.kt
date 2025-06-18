d9cf991bb511f1f0ca4cda5bfb8930cb11002ed4
mygomii <komijeong91@gmail.com>
2025-06-18 19:57:53 +0900

    # push test

--- app/src/main/java/com/mygomii/dictionary/WordApp.kt
+++ app/src/main/java/com/mygomii/dictionary/WordApp.kt
@@ -10,7 +10,7 @@ class WordApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        println("push-test")
+        println("push-test-1")
 
         startKoin {
             androidContext(this@WordApp)
